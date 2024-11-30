05c99f15bfd061381752e4c90bb35b6621c72495
littlebai086 <peter777200067@gmail.com>
2024-11-30 14:51:26 +0800

    # commit

--- download_youtube.py
+++ download_youtube.py
@@ -11,7 +11,11 @@ video_url = "https://youtube.com/watch?v=gd38-X3HpbM"
 
 ydl_opts = {
     'format': 'bestvideo+bestaudio/best',
-    'outtmpl': './%(title)s.%(ext)s'
+    'outtmpl': './%(title)s.%(ext)s',  # 设置输出文件的名称和扩展名
+    'postprocessors': [{  # 使用 postprocessor 转换格式
+        'key': 'FFmpegVideoConvertor',  # 使用 FFmpeg 进行转换
+        'preferedformat': 'mp4',  # 设置目标格式为 MP4
+    }],
 }
 
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
